8a904c0528bac9a8cc528d267d0f25c292479d50
jdjuan <david.juanherrera@gmail.com>
2018-09-25 22:09:33 -0500

    feat: add timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,32 @@
+import { AppConfig } from '../environments/environment';
 import { Component } from '@angular/core';
 import { ElectronService } from './providers/electron.service';
+import { Observable } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
-import { AppConfig } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(public electronService: ElectronService,
-    private translate: TranslateService) {
-
+  constructor(
+    public electronService: ElectronService,
+    private translate: TranslateService,
+  ) {
     translate.setDefaultLang('en');
     console.log('AppConfig', AppConfig);
 
     if (electronService.isElectron()) {
       console.log('Mode electron');
-      console.log('Electron ipcRenderer', electronService.ipcRenderer);
-      console.log('NodeJS childProcess', electronService.childProcess);
+      console.log(
+        'Electron ipcRenderer',
+        electronService.ipcRenderer,
+      );
+      console.log(
+        'NodeJS childProcess',
+        electronService.childProcess,
+      );
     } else {
       console.log('Mode web');
     }
